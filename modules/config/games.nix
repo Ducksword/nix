a8478f45eac451d022f04322657f16fcc609f1d2
ducksword <Diabotek@proton.me>
2024-12-29 15:12:53 -0500

    changed heroic version

--- modules/config/games.nix
+++ modules/config/games.nix
@@ -11,6 +11,6 @@
   # Enable heroic games
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
-  environment.systemPackages = [pkgs.heroic];
+  environment.systemPackages = [pkgs.heroic-unwrapped];
 
 }
