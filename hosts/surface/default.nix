32d12d11498c775824bcee8a3ccf2e2949221a23
ducksword <Diabotek@proton.me>
2025-01-03 11:24:27 -0500

    vscode testing

--- hosts/surface/default.nix
+++ hosts/surface/default.nix
@@ -1,7 +1,21 @@
-{ ... }:
+{  pkgs, ... }:
 {
   imports = [
     ../../modules/default.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+
+    (vscode-with-extensions.override {
+        vscodeExtensions = with vscode-extensions; [
+          jnoortheen.nix-ide
+          nefrob.vscode-just-syntax
+          christian-kohler.path-intellisense
+          mhutchie.git-graph
+          ms-vscode-remote.remote-ssh
+          ms-vscode-remote.remote-ssh-edit
+        ];
+    })
   ];
 
   # Enable the OpenSSH daemon.
