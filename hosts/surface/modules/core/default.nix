8dbed07a684ba6f24288dc05946866701679ac4f
Ducksword <Diabotek@proton.me>
2025-01-08 00:53:07 -0500

    added locale.nix

--- hosts/surface/modules/core/default.nix
+++ hosts/surface/modules/core/default.nix
@@ -3,5 +3,18 @@
   imports = [
     ./users.nix
     ./ssh.nix
+    ./locale.nix
   ];
+
+  # Enable flakes
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable firmware updates
+  services.fwupd.enable = true;
 }
