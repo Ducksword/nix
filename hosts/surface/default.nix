e65069f5872feef6a44e16630e0f3768c49c9f2d
ducksword <Diabotek@proton.me>
2025-01-09 12:52:48 -0500

    added floorp.nix

--- hosts/surface/default.nix
+++ hosts/surface/default.nix
@@ -4,16 +4,11 @@
     ../../modules/core/default.nix
     ../../modules/desktops/default.nix
     ../../modules/utils/pack/desktop.nix
+    ../../modules/applications/pack/desktop.nix
   ];
   # Desktop
   gnome.enable = true;
 
   # Enable zram
   zramSwap.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    floorp
-  ];
 }
